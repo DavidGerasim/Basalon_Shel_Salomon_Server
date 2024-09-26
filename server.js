05ff78eee6c4ccc0124b86e83af0f757e2fc5ac3
vladgrisko <vladislav.grisko@biocatch.com>
2024-09-26 14:57:56 +0300

    added disclosue of current server ip

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-require("./config/db"); // Ensure the database connection is established
+require("./config/db");
 
 const express = require("express");
+const os = require("os");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const UserRouter = require("./api/User");
 
-app.use(express.json()); // Middleware to parse JSON bodies
-
-app.use("/user", UserRouter); // Mount the UserRouter at /user
+app.use(express.json());
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  const networkInterfaces = os.networkInterfaces();
+  const ip = networkInterfaces['en0']?.find(i => i.family === 'IPv4')?.address || 'localhost';
+  console.log(`Server running at http://${ip}:${port}`);
 });
+
